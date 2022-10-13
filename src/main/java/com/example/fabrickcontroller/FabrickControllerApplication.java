900721d8f241886e2faee8f4ce3364d23b57cc61
Mattia Failla <mattiafailla@hotmail.it>
2022-10-13 19:26:19 +0200

    Add boilerplate

---
+++ src/main/java/com/example/fabrickcontroller/FabrickControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabrickcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabrickControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fabrickcontroller/FabrickControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabrickcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
