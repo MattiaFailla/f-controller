d35fd21e3a05e9d0e8b309356e8896768e66d921
Mattia Failla <mattiafailla@hotmail.it>
2022-10-20 19:41:44 +0200

    Test Account Service

---
+++ src/test/java/com/example/fabrickcontroller/AccountTest.java
@@ -0,0 +1,55 @@
+package com.example.fabrickcontroller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AccountTest extends FabrickControllerApplicationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/f/accounts/14537780/balance")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Api-Key", "")
+                .header("Auth-Schema", "S2S")
+        ).andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    public void testGetAccount() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/f/accounts/14537780")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Api-Key", "")
+                .header("Auth-Schema", "S2S")
+        ).andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    public void testGetTransactions() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/f/accounts/14537780/transactions?fromAccountingDate=2022-10-01&toAccountingDate=2022-10-20")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Api-Key", "")
+                .header("Auth-Schema", "S2S")
+        ).andExpect(status().isOk()).andDo(print());
+    }
+
+
+}
--- src/test/java/com/example/fabrickcontroller/FabrickControllerApplicationTests.java
+++ src/test/java/com/example/fabrickcontroller/FabrickControllerApplicationTests.java
@@ -1,9 +1,18 @@
 package com.example.fabrickcontroller;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application.properties"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FabrickControllerApplicationTests {
 
     @Test
